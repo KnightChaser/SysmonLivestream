0a03a094ecc461198584ff6cdec444d8452aab23
Lee Garam <agerio100@naver.com>
2024-03-24 00:21:49 +0900

    Structuring with class

--- main.py
+++ main.py
@@ -3,25 +3,35 @@ import subprocess
 import re
 import time
 
-# Query logman for the GUID of the provider
-def get_etw_guid(provider:str) -> str:
-    cmd = f"logman query providers {provider}"
-    output = subprocess.check_output(cmd, shell=True).decode()
-    guid = re.search(r'{.*}', output).group(0)
-    return guid
+class SysmonETWSession(etw.ETW):
 
-def etw_callback(event:any) -> None:
-    print(event)
+    # Initializing the ETW session for Sysmon
+    def __init__ (self, sysmon_provider:str = "Microsoft-Windows-Sysmon") -> None:
+        self.sysmon_provider = sysmon_provider
+        self.sysmon_provider_guid = self.get_etw_guid(self.sysmon_provider)
+        self.etw_sysmon_provider = [etw.ProviderInfo(self.sysmon_provider, etw.GUID(self.sysmon_provider_guid))]
+        super().__init__(providers=self.etw_sysmon_provider, event_callback=self.etw_callback)
 
+    # Get the GUID of the ETW provider
+    def get_etw_guid(self, provider:str) -> str:
+        cmd = f"logman query providers {provider}"
+        output = subprocess.check_output(cmd, shell=True).decode()
+        guid = re.search(r'{.*}', output).group(0)
+        return guid
+    
+    # Callback function to print the ETW events
+    def etw_callback(self, event:any) -> None:
+        print(event)
 
-def start(provider:str) -> None:
-    provider_guid = get_etw_guid(provider)
-    print(f"Provider: {provider}")
-    print(f"Provider GUID: {provider_guid}")
-    etw_sysmon_provider = [etw.ProviderInfo(provider, etw.GUID(provider_guid))]
-    job = etw.ETW(providers=etw_sysmon_provider, event_callback=etw_callback)
-    job.start()
-    time.sleep(5)
-    job.stop()
+    # Start the ETW session
+    def run(self, testing=True):
+        self.start()
+        if testing:
+            time.sleep(5)
+            self.stop()
+        else:
+            pass
 
-start("Microsoft-Windows-Sysmon")
+if __name__ == "__main__":
+    sysmon_etw = SysmonETWSession()
+    sysmon_etw.run(testing=True)
