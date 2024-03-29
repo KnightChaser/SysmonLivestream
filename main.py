1f2ae578b2f2867d135baee337581834199a8a6b
Garam Lee <47494005+KnightChaser@users.noreply.github.com>
2024-03-29 19:38:08 +0900

    Deployment bug fixes for standalone building
    Corrected mishandled directory and system arguments, etc.

--- main.py
+++ main.py
@@ -30,19 +30,14 @@ if __name__ == "__main__":
     if args.opensearch and not args.endpoint and not args.account and not args.index:
         argument_parser.error("--endpoint, --account, --index are required options when --opensearch is enabled")
 
-    # Set option to SysmonETWSession
-    if args.opensearch:
-        sysmon_etw = SysmonETWSession(service = "opensearch", 
-                                      endpoint = args.endpoint,
-                                      account = args.account,
-                                      index = args.index)
-    else:
-        sysmon_etw = SysmonETWSession()
-    
     # Run ./deploySysmon.ps1 to check the status of the Sysmon service
-    current_directory = os.path.abspath(os.getcwd())
-    script_directory = os.path.dirname(os.path.abspath(__file__))
-    powershell_execution_command = f"powershell.exe -ExecutionPolicy RemoteSigned -file {script_directory}/deploySysmon.ps1"
+    if getattr(sys, "frozen", False):
+        script_path = os.path.join(sys._MEIPASS, "scripts/deploySysmon.ps1")
+        powershell_execution_command = f"powershell.exe -ExecutionPolicy RemoteSigned -file {script_path}"
+    else:
+        script_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "scripts/deploySysmon.ps1")
+        powershell_execution_command = f"powershell.exe -ExecutionPolicy RemoteSigned -file {script_path}"
+
     if args.checkAndDeploySysmonOffline:
         if len(args.checkAndDeploySysmonOffline.split(":")) != 2:
             argument_parser.error("--deploy option requires sysmonExePath and sysmonConfigPath. (sysmonExePath:sysmonConfigPath)")
@@ -53,7 +48,25 @@ if __name__ == "__main__":
     else:
         powershell_execution_command += " -checkAndDeploySysmonOffline false"
 
-    checker = subprocess.Popen(powershell_execution_command, stdout = sys.stdout)
-    checker.communicate()
+    try:
+        checker = subprocess.Popen(powershell_execution_command, stdout = sys.stdout)
+        checker.communicate()[0]
+        checker.wait()
+        if checker.returncode != 0:
+            print("Error during running Sysmon service checkup. Is the programming running with administrator privilege?")
+            sys.exit(1) 
 
-    sysmon_etw.run(testing = args.testing)
+        # Set option to SysmonETWSession
+        # Because SysmonETWSession requires Sysmon service registerd to logman, the previous step is necessary.
+        if args.opensearch:
+            sysmon_etw = SysmonETWSession(service = "opensearch", 
+                                        endpoint = args.endpoint,
+                                        account = args.account,
+                                        index = args.index)
+        else:
+            sysmon_etw = SysmonETWSession()
+
+        sysmon_etw.run(testing = args.testing)
+    except Exception as exception:
+        print(f"Error during running script: {exception}")
+        sys.exit(1)
